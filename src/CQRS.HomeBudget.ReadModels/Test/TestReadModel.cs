931d7688a4d1c0dbf4bf5b140841726408ca8831
Marek Frankowicz <frankowicz.marek@gmail.com>
2019-06-16 15:20:46 +0200

    Set up read models project with test read model

---
+++ src/CQRS.HomeBudget.ReadModels/Test/TestReadModel.cs
@@ -0,0 +1,19 @@
+﻿using CQRS.HomeBudget.Domain.Test;
+using CQRS.HomeBudget.Domain.Test.Events;
+using EventFlow.Aggregates;
+using EventFlow.ReadStores;
+
+namespace CQRS.HomeBudget.ReadModels.Test
+{
+    public class TestReadModel : IReadModel, IAmReadModelFor<TestAggregate, TestAggregateId, TestCreated>
+    {
+        public string Id { get; set; }
+        public string TestName { get; set; }
+
+        public void Apply(IReadModelContext context, IDomainEvent<TestAggregate, TestAggregateId, TestCreated> testCreated)
+        {
+            Id = testCreated.AggregateIdentity.Value;
+            TestName = testCreated.AggregateEvent.TestName;
+        }
+    }
+}
