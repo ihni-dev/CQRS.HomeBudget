26dae083cb26d2e7f0bd45e91a227a74d5ce49bb
Marek Frankowicz <frankowicz.marek@gmail.com>
2019-06-16 15:21:01 +0200

    Set up composition root project

---
+++ src/CQRS.HomeBudget.CompositionRoot/EventFlowOptionsExtensions.cs
@@ -0,0 +1,15 @@
+﻿using CQRS.HomeBudget.CompositionRoot.EventFlowModules;
+using EventFlow;
+
+namespace CQRS.HomeBudget.CompositionRoot
+{
+    public static class EventFlowOptionsExtensions
+    {
+        public static IEventFlowOptions RegisterModules(this IEventFlowOptions eventFlowOptions)
+        {
+            eventFlowOptions.RegisterModule(new TestModule());
+
+            return eventFlowOptions;
+        }
+    }
+}
